1f4d50f88b30cf237bac92be72e90b8cd929e5b4
EdvardBM <edvardbm@ntnu.no>
2023-06-22 19:43:01 +0200

    m

--- ros2_ws/src/edvard_coppeliaSim/scripts/IDUN_scripts/end_game_rt_12_smaller_box copy.py
+++ ros2_ws/src/edvard_coppeliaSim/scripts/IDUN_scripts/end_game_rt_12_smaller_box copy.py
@@ -24,7 +24,7 @@ SCENE_FILE = join(dirname(abspath(__file__)),
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-EPISODES = 10000
+EPISODES = 20000
 EPISODE_LENGTH = 200
 LAST_EPISODES_MEMORY = 2
 SAVE_EVERY_X_EPISODE = 250
@@ -35,7 +35,7 @@ HEADLESS = False
 BASE_DIR = 'End_game'
 RUN_NAME = 'Run_GPU_12'
 LOAD_PREVIOUS_RUN = True
-EPISODE_NUM = 'Episode_430'
+EPISODE_NUM = 'Episode_1379'
 PREVIOUS_RUN_PATH = f"/root/{BASE_DIR}/{RUN_NAME}/{EPISODE_NUM}/model.pth"
 
 WANDB_CONTINUE = LOAD_PREVIOUS_RUN 
